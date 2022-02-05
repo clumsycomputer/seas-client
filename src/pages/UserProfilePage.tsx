763858db6bfe219c63baabd66479feefc52707d5
jmath <clumsycomputer@gmail.com>
2022-02-05 13:44:55 -0700

    feature(./src/pages/UserProfilePage.tsx): update UserProfilePage after list deleted

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -22,12 +22,35 @@ export function UserProfilePage() {
   const navigateToPage = useNavigate()
   const currentUser = useCurrentUser()
   const [pageBody, setPageBody] = useState<ReactNode>(null)
-  useEffect(() => {
-    SeasService.getUserProfile({
+  const [getUserProfileState, getUserProfile] = useTask(async () => {
+    const userProfileData: unknown = SeasService.getUserProfile({
       userId: routeParams.userId!,
-    }).then((userProfileData: unknown) => {
-      const userProfile = userProfileData as UserProfile
-      const currentUserCanEditProfile = currentUser?.id === userProfile.id
+    })
+    const userProfile = userProfileData as UserProfile
+    return userProfile
+  })
+  const [deleteUserProfileState, deleteUserProfile] = useTask(
+    async (contentListId: string) => {
+      if (currentUser) {
+        SeasService.deleteContentList({
+          authToken: currentUser.authToken,
+          contentListId: contentListId,
+        })
+      }
+    }
+  )
+  useEffect(() => {
+    getUserProfile()
+  }, [routeParams.userId])
+  useEffect(() => {
+    if (deleteUserProfileState.taskStatus === 'taskSuccessful') {
+      getUserProfile()
+    }
+  }, [deleteUserProfileState])
+  useEffect(() => {
+    if (getUserProfileState.taskStatus === 'taskSuccessful') {
+      const userProfile = getUserProfileState.taskResult
+      const currentUserCanEditProfile = userProfile.id === currentUser?.id
       setPageBody(
         currentUserCanEditProfile ? (
           <EditableUserProfileDisplay
@@ -39,12 +62,7 @@ export function UserProfilePage() {
               navigateToPage(`/content-list/${someContentList.id}/edit`)
             }}
             deleteContentList={(someContentList) => {
-              SeasService.deleteContentList({
-                authToken: currentUser.authToken,
-                contentListId: someContentList.id,
-              }).then(() => {
-                // todo refresh user profile
-              })
+              deleteUserProfile(someContentList.id)
             }}
           />
         ) : (
@@ -55,8 +73,8 @@ export function UserProfilePage() {
           />
         )
       )
-    })
-  }, [routeParams.userId])
+    }
+  }, [getUserProfileState])
   return currentUser === null ? (
     <LoggedOutUserPage pageBody={pageBody} />
   ) : (
@@ -195,4 +213,49 @@ function UserProfileDisplay(props: UserProfileDisplayProps) {
       </List>
     </Stack>
   )
+}
+
+type TaskState<TaskResult> =
+  | {
+      taskStatus: 'taskNotInitialized'
+    }
+  | { taskStatus: 'taskActive' }
+  | {
+      taskStatus: 'taskSuccessful'
+      taskResult: TaskResult
+    }
+  | { taskStatus: 'taskError'; taskError: any }
+
+function useTask<
+  SomeTask extends (...args: Array<any>) => Promise<any> = (
+    ...args: Array<any>
+  ) => Promise<any>,
+  TaskResult = ReturnType<SomeTask> extends Promise<infer T> ? T : never
+>(
+  someTask: SomeTask
+): [TaskState<TaskResult>, (...args: Parameters<typeof someTask>) => void] {
+  const [taskState, setTaskState] = useState<TaskState<TaskResult>>({
+    taskStatus: 'taskNotInitialized',
+  })
+  const triggerTask: (...args: Parameters<typeof someTask>) => void = (
+    ...args
+  ) => {
+    someTask(...args)
+      .then((taskResult) => {
+        setTaskState({
+          taskResult,
+          taskStatus: 'taskSuccessful',
+        })
+      })
+      .catch((taskError) => {
+        setTaskState({
+          taskError,
+          taskStatus: 'taskError',
+        })
+      })
+    setTaskState({
+      taskStatus: 'taskActive',
+    })
+  }
+  return [taskState, triggerTask]
 }
